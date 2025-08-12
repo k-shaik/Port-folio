b51fdc701c1d597790d89aaaaf6fe7f5dfd7240d
Mohammad Khaja Shaik <92737226+k-shaik@users.noreply.github.com>
2025-08-13 01:15:19 +0530

    first commit

---
+++ js/commands.js
@@ -0,0 +1,177 @@
+var password = "123456";
+var email = 'mailto:khajashaik070404@gmail.com';
+
+var whois = [
+  "<br>",
+  "Who am I? I'm like a pixel in the digital universe, a byte-sized comedian ready to tickle your funny bone..",
+  "<br>"
+];
+
+var whoami = [
+  "<br>",
+  "Hey, I'm Khaja_Shaik👋",
+  "a Computer Science Graduate from KL University passionate about strengthening digital defenses and solving complex security challenges.",
+  "I specialize in penetration testing, vulnerability assessments, and OWASP Top 10 best practices, using tools like Nmap, Burp Suite, and Python automation.",
+  "As President of the KL SAC Cybersecurity Club and winner of INNOTHON 24, I combine technical expertise with leadership to build secure, resilient systems.",
+  "<br>"
+];
+
+var experience = [
+  "<br>",
+  "<span class='command'>Team Lead</span>",
+  "2023-2024",
+  "KL SAC Cybersecurity Club",
+  "Led the club’s cybersecurity initiatives, conducting 10+ workshops on social engineering and cyber awareness for 100+ students each.",
+  "<br>",
+  "<span class='command'>Tech Lead</span>",
+  "2023-2024",
+  "EFIT",
+  "Guided technical teams in delivering impactful community projects and initiatives within EFIT.",
+  "<br>",
+  "<span class='command'>Head Mentor</span>",
+  "2023-2024",
+  "Forensixplore Club",
+  "Mentored students in digital forensics and cybersecurity, fostering hands-on learning and investigative skills.",
+  "<br>"
+];
+
+var education = [
+  "<br>",
+  "<span class='command'>Bachelor of Technology in Computer Science & Information Technology</span>",
+  "2021 - Present",
+  "Koneru Lakshmaiah Education Foundation (KL University)",
+  "CGPA: 9.15",
+  "<br>",
+  "<span class='command'>Board of Intermediate Education</span>",
+  "2019 - 2021",
+  "NRI Educational Society",
+  "Score: 650/1000",
+  "<br>",
+  "<span class='command'>Secondary School Certificate</span>",
+  "2018 - 2019",
+  "St. Luke’s School",
+  "CGPA: 8.02",
+  "<br>"
+];
+
+var skills = [
+  "<br>",
+  "<span class='command'>Programming Languages:</span> Java",
+  "<span class='command'>Network Security:</span> Firewall configuration, Intrusion Detection/Prevention Systems, VPN implementation, Network segmentation",
+  "<span class='command'>Cryptographic Protocols:</span> TLS/SSL, AES encryption, Public Key Infrastructure",
+  "<span class='command'>Vulnerability Assessment & Penetration Testing:</span> Nmap, Metasploit, OWASP Top 10",
+  "<br>"
+];
+
+var projects = [
+  "<br>",
+  "<span class='command'>InosKL24 - OSINT Toolkit</span>",
+  "Your All-in-One OSINT Toolkit for Security & Privacy Management!",
+  "Python-based toolkit with features like data breach checks, domain/IP info retrieval, phone number lookup, leaked GitHub email search, social media OSINT, and malware analysis.",
+  "<a href='https://github.com/k-shaik/INOSKL-24' target='_blank'>View on GitHub</a>",
+  "<br>",
+  
+  "<span class='command'>Adversary-Aware Deepfake Detection</span>",
+  "A robust, ensemble-based deepfake detection framework integrating CNNs and Transformers, with adaptive weighting and noise-robust learning for real-world adversarial scenarios.",
+  "Published in Scopus-indexed journal, achieving 96.4% accuracy and strong cross-dataset generalization.",
+  "<a href='https://doi.org/10.1007/s11042-024-20548-6' target='_blank'>Read Paper</a>",
+  "<br>",
+  
+  "<span class='command'>Academic Website Security Audit</span>",
+  "March 2023 – April 2023",
+  "Performed penetration testing on the university's academic website, identifying and documenting critical vulnerabilities, leading to immediate security fixes.",
+  "<a href='https://drive.google.com/file/d/1TcWaw1Nf4pErWmpCMcnMc62iaoOE7jIO/view?usp=sharing' target='_blank'>View Report</a>",
+  "<br>",
+  
+  "<span class='color2'>To explore more projects, visit my GitHub:</span> <a href='https://github.com/k-shaik' target='_blank'>github.com/k-shaik</a>",
+  "<br>"
+];
+
+var cert = [
+  "<br>",
+  " ██████╗ ███████╗ ██╗  ██╗",
+  "██╔════╝ ██╔════╝ ██║  ██║",
+  "██║      █████╗   ███████║",
+  "██║      ██╔══╝   ██╔══██║",
+  "╚██████╗ ███████╗ ██║  ██║",
+  " ╚═════╝ ╚══════╝ ╚═╝  ╚═╝",
+  "~~~~~~~~~~~~~~~~~~~~~~~~",
+ "<a href='https://aspen.eccouncil.org/Verify' target='_blank'>Certified Ethical Hacker (CEH v11) - EC-Council</a>",
+   "<br>",
+
+" █████╗ ███████╗      █████╗  ██████╗  ██████╗ ",
+"██╔══██╗╚══███╔╝     ██╔══██╗██╔═████╗██╔═████╗",
+"███████║  ███╔╝█████╗╚██████║██║██╔██║██║██╔██║",
+"██╔══██║ ███╔╝ ╚════╝ ╚═══██║████╔╝██║████╔╝██║",
+"██║  ██║███████╗      █████╔╝╚██████╔╝╚██████╔╝",
+"╚═╝  ╚═╝╚══════╝      ╚════╝  ╚═════╝  ╚═════╝ ",
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+ "<a href='https://www.credly.com/badges/3187d6c7-7560-4722-8b8f-61f136cd9bf7/public_url' target='_blank'>Azure Fundamentals (AZ-900) - Microsoft</a>",
+
+
+  "<br>",
+
+];
+
+
+var contact = [
+  "<br>",
+  "📧 Email: <a href='mailto:Kshaik.one@gmail.com' target='_blank'>Kshaik.one@gmail.com</a>",
+  "💼 LinkedIn: <a href='https://www.linkedin.com/in/kshaik1/' target='_blank'>linkedin.com/in/kshaik1/</a>",
+  "🐙 GitHub: <a href='https://github.com/k-shaik' target='_blank'>github.com/k-shaik</a>",
+  "🌐 Portfolio: <a href='https://kshaik-web.web.app/' target='_blank'>kshaik-web.web.app</a>",
+  "<br>"
+];
+
+
+var secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+var CV = [
+  "<br>",
+  'My Resume         <a href="https://drive.google.com/file/d/1TcWaw1Nf4pErWmpCMcnMc62iaoOE7jIO/view?usp=sharing" target="_blank">Click Here</a>',
+  "<br>"
+  
+];
+
+
+var ls = [
+  "<br>",
+  '<span class="command">whois</span>',
+  '<span class="command">whoami</span>         Who is Khaja_shaik?',
+  '<span class="command">experience</span>     View work and leadership experience',
+  '<span class="command">education</span>      View academic background',
+  '<span class="command">skills</span>         What skills do I possess?',
+  '<span class="command">projects</span>       View coding and research projects',
+  '<span class="command">cert</span>           See My Certifications Here',
+  '<span class="command">contact</span>        View contact details',
+  '<span class="command">CV</span>             Display My Resume',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+
+var banner = [
+  '<span class="index">Khaja Shaik(AC) Not A Corporation. All knights reserved.</span>',
+
+
+ " ██████╗░░█████╗░██████╗░████████╗███████╗░█████╗░██╗░░░░░██╗░█████╗░  ██████╗░░█████╗░░██████╗░███████╗",
+ " ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██║░░░░░██║██╔══██╗  ██╔══██╗██╔══██╗██╔════╝░██╔════╝",
+  "██████╔╝██║░░██║██████╔╝░░░██║░░░█████╗░░██║░░██║██║░░░░░██║██║░░██║  ██████╔╝███████║██║░░██╗░█████╗░░",
+ " ██╔═══╝░██║░░██║██╔══██╗░░░██║░░░██╔══╝░░██║░░██║██║░░░░░██║██║░░██║  ██╔═══╝░██╔══██║██║░░╚██╗██╔══╝░░",
+ " ██║░░░░░╚█████╔╝██║░░██║░░░██║░░░██║░░░░░╚█████╔╝███████╗██║╚█████╔╝  ██║░░░░░██║░░██║╚██████╔╝███████╗",
+  "╚═╝░░░░░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░░╚════╝░╚══════╝╚═╝░╚════╝░  ╚═╝░░░░░╚═╝░░╚═╝░╚═════╝░╚══════╝",
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+ 
+  
+  '<span class="color2">Welcome to my portfolio web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'ls'</span><span class=\"color2\">.</span>",
+];
+
